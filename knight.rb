7ae3c4a6a24877607587c2abcd1347a2e090d38e
mogs.git <mogs.mail1@gmail.com>
2020-05-18 10:45:50 +0100

    clean up code

--- knight.rb
+++ knight.rb
@@ -1,4 +1,5 @@
 require 'matrix'
+
 class Vertex
 	attr_accessor :position, :neighbours
 	include Comparable
@@ -21,18 +22,17 @@ class Vertex
 		moves = []
 		[1,-1].each do |num1| 
 			[2,-2].each do |num2|
-				moves.push([num1, num2])
+				moves.push(Vector::elements([num1, num2], copy = true))
+				moves.push(Vector::elements([num2, num1], copy = true))
 			end
 		end
-		# puts moves.to_s	
-		moves.length.times {|i| moves.push(moves[i].reverse)}
-		return moves.map {|move| Vector::elements(move, copy=true)}
+
+		return moves
 	end
 
 	def find_neighbours
 		new_positions = []
 		self.move_vectors.each {|vector| new_positions.push(vector + self.position)}
-		# puts new_positions.to_s
 		valid_positions = new_positions.keep_if {|position| position[0].between?(0,7) && position[1].between?(0,7)}
 		return valid_positions
 	end
@@ -43,42 +43,43 @@ class Vertex
 
 end
 
-def print_levels levels
-	puts
-	levels.each_with_index{|level, i| puts "Level #{i}: #{print_array(level)}"}
-	puts
-end
 def print_array arr 
 	s = ""
 	arr.each{|el| s += "#{el.to_s}, "}
 	return s
 end
 
-start_pos = Vertex.new(3,3)
-finish_pos = Vertex.new(3, 4)
+def print_levels levels
+	puts
+	levels.each_with_index{|level, i| puts "Level #{i}: #{print_array(level)}"}
+	puts
+end
 
 def construct_levels (start_pos, finish_pos)
 	levels = [[start_pos]]
 	until levels.last.include?(finish_pos)
+		# Get the neighbour positions for every valid position of the knight
 		neighbour_vertices = []
 		levels.last.each{|vertex| neighbour_vertices.push(vertex.neighbours)}
+		
+		# Convert these position vectors to vertices. Keep only if the knight hasn't travelled there before.
 		neighbour_vertices = neighbour_vertices.flatten.map{|pos| Vertex.new(pos[0], pos[1])}
 		neighbour_vertices = neighbour_vertices.keep_if {|vertex| !levels.flatten.include?(vertex)}
+
+		# Remove any duplicated positions.
 		neighbour_vertices = neighbour_vertices.uniq(&:position)
+
+		# The remaining Vertices are all the valid positions the Knight could move to. Add these to the next level.
 		levels.push(neighbour_vertices)
 	end
-	levels.last.keep_if{|vertex| vertex == finish_pos}
+
+	levels.last.keep_if{|vertex| vertex == finish_pos} # The last level should just contain the finish position.
 	return levels
 end
 
-levels = construct_levels(start_pos, finish_pos)
-print_levels(levels)
-
-# puts levels[levels.length-2].keep_if {|vertex| levels.last[0].neighbours.include?(vertex.position)}
-
-# puts levels.last[0].neighbours[0] == Vertex.new(4,6).position
-
 def prune_levels (levels)
+	# From the final level (the finish position), iterate back through each level and 
+	# remove vertices that aren't a neighbour position of the calling level. 
 	i = levels.length-1
 	while i > 0
 		parent_level_neighbours = []
@@ -89,24 +90,15 @@ def prune_levels (levels)
 	return levels
 end
 
-print_levels(prune_levels (levels))
-
-master_array = []
-
-i = levels.length-1
-
-def build_paths_print (parents, children)
-	paths = []
-	parents.each do |parent|
-		valid_children = children.keep_if{|child| parent.neighbours.include?(child.position)}
-		valid_children.each {|child| paths.push([parent.position, child.position])}
-	end
-	return paths
-end
-
 def build_paths (levels)
 	i = levels.length-1
-	paths = [levels.last]
+	paths = [levels.last] 
+
+	# paths begins as a single array of finish_pos, this array is then dequeued, and 
+	# paths becomes n_children arrays of [finish_pos, nth_child_pos]
+	# children to choose from come from the next level closer to the start_pos, a paths last vertex
+	# might have multiple valid children in the next level, in which case all are pushed as separate new paths. 
+
 	while i > 0
 		children = levels[i-1]
 		current_paths = paths.length
@@ -115,22 +107,30 @@ def build_paths (levels)
 			# each parent is an array or path, which needs to be extended using the children of the last vertex in the path
 			parent = path.last 
 			valid_children = children.keep_if{|child| parent.neighbours.include?(child.position)}
-			puts "valid children"
-			valid_children.each{|child| puts child.position}
 			valid_children.each {|child| new_paths.push(path + [child])}
 		end
 		paths = new_paths 
 		i -= 1
 	end
+
+	return paths
+end
+
+def print_paths (paths)
 	paths.each do |el| 
 		puts
 		el.each do |vert| 
 			puts vert.position
 		end
 	end
-	return paths
 end
 
-# puts levels.last.to_s
-# build_paths(levels).each {|path| puts path.length} 
-build_paths(levels)
+start_pos = Vertex.new(0,0)
+finish_pos = Vertex.new(5, 7)
+
+levels = construct_levels(start_pos, finish_pos)
+# print_levels(levels)
+levels = prune_levels(levels)
+# print_levels(levels)
+paths = build_paths(levels)
+print_paths(paths)
